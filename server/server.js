1f636f86474da53f75ecc3366cc0d49ffdcdc527
2ndjoy <rahulchakrabartyjoy15@gmail.com>
2026-05-12 23:41:02 +0600

    Setup backend auth and MongoDB

--- server/server.js
+++ server/server.js
@@ -1,5 +1,18 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+
 const app = require("./app");
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
-});
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB Connected");
+
+        app.listen(5000, () => {
+            console.log("Server running on port 5000");
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
