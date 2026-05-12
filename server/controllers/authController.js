feaa36d05530ceeab318e700bbe198752371e22b
2ndjoy <rahulchakrabartyjoy15@gmail.com>
2026-05-12 23:53:44 +0600

    Setup backend auth and MongoDB

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,6 +5,7 @@ exports.register = async (req, res) => {
     try {
         const { name, email, password } = req.body;
 
+        // Check existing user
         const existingUser = await User.findOne({ email });
 
         if (existingUser) {
@@ -13,14 +14,17 @@ exports.register = async (req, res) => {
             });
         }
 
+        // Hash password
         const hashedPassword = await bcrypt.hash(password, 10);
 
+        // Create new user
         const user = await User.create({
             name,
             email,
             password: hashedPassword,
         });
 
+        // Response
         res.status(201).json({
             message: "User registered successfully",
             user,
@@ -31,4 +35,55 @@ exports.register = async (req, res) => {
             message: error.message,
         });
     }
+};
+const jwt = require("jsonwebtoken");
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // Find user
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({
+                message: "Invalid email or password",
+            });
+        }
+
+        // Compare password
+        const isMatch = await bcrypt.compare(
+            password,
+            user.password
+        );
+
+        if (!isMatch) {
+            return res.status(400).json({
+                message: "Invalid email or password",
+            });
+        }
+
+        // Generate JWT token
+        const token = jwt.sign(
+            {
+                id: user._id,
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "7d",
+            }
+        );
+
+        res.status(200).json({
+            message: "Login successful",
+            token,
+            user,
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+
 };
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,8 +4,11 @@ const router = express.Router();
 
 const {
     register,
+    login,
 } = require("../controllers/authController");
 
 router.post("/register", register);
+
+router.post("/login", login);
 
 module.exports = router;
